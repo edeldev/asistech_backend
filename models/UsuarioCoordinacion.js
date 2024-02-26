00e79088f1a74f3e1f01cc1d8a432b832786d7a6
edeldev <edel@ellyonsoft.com>
2024-02-25 18:07:20 -0600

    ajustes

--- models/UsuarioCoordinacion.js
+++ models/UsuarioCoordinacion.js
@@ -1,45 +1,50 @@
 import mongoose from "mongoose";
-import bcrypt from 'bcrypt'
+import bcrypt from "bcrypt";
 
 // Maestros
-const usuarioSchemaCoordinacion = mongoose.Schema({
+const usuarioSchemaCoordinacion = mongoose.Schema(
+  {
     nombre: {
-        type: String,
-        required: true,
-        trim: true,
+      type: String,
+      required: true,
+      trim: true,
     },
     password: {
-        type: String,
-        required: true,
-        trim: true,
+      type: String,
+      required: true,
+      trim: true,
     },
     matricula: {
-        type: String,
-        required: true,
-        trim: true,
-        unique: true,
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
     },
     token: {
-        type: String,
+      type: String,
     },
-}, 
-    {
-        timestamps: true,
-    }
+  },
+  {
+    timestamps: true,
+  }
 );
-usuarioSchemaCoordinacion.pre('save', async function(next) {
-    if(!this.isModified("password")) {
-        next()
-    }
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt)
-})
+usuarioSchemaCoordinacion.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
-usuarioSchemaCoordinacion.methods.comprobarPassword = async function(passwordFormulario){
-    return await bcrypt.compare(passwordFormulario, this.password)
-}
+usuarioSchemaCoordinacion.methods.comprobarPassword = async function (
+  passwordFormulario
+) {
+  return await bcrypt.compare(passwordFormulario, this.password);
+};
 
-const UsuarioCoordinacion = mongoose.model("Coordinadores", usuarioSchemaCoordinacion)
+const UsuarioCoordinacion = mongoose.model(
+  "Coordinadores",
+  usuarioSchemaCoordinacion
+);
 
-
-export default UsuarioCoordinacion
+export default UsuarioCoordinacion;
