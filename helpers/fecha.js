5e45b51384c2e3f27e67001af17f2ef63e7a9378
edeldev <edel@ellyonsoft.com>
2024-03-22 13:00:14 -0600

    cambio zona horaria

--- helpers/fecha.js
+++ helpers/fecha.js
@@ -1,42 +1,45 @@
 const obtenerFechaFormateada = () => {
-    const fechaActual = new Date();
-    const formatoFecha = new Intl.DateTimeFormat('es-MX', {
-        weekday: 'long',
-        year: 'numeric',
-        month: 'long',
-        day: '2-digit',
-        hour: '2-digit',
-        minute: '2-digit',
-        second: '2-digit',
-        hour12: true,
-        timeZone: 'America/Mexico_City',
-    });
+  const fechaActual = new Date();
+  const formatoFecha = new Intl.DateTimeFormat("es-MX", {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: true,
+    timeZone: "America/Monterrey",
+  });
 
-    return formatoFecha.formatToParts(fechaActual).reduce((acc, part) => {
-        acc[part.type] = part.value;
-        return acc;
-    }, {});
+  return formatoFecha.formatToParts(fechaActual).reduce((acc, part) => {
+    acc[part.type] = part.value;
+    return acc;
+  }, {});
 };
 
 const obtenerNombreMes = () => {
-    const fechaActual = new Date();
-    return new Intl.DateTimeFormat('es', { month: 'long' }).format(fechaActual);
+  const fechaActual = new Date();
+  return new Intl.DateTimeFormat("es", { month: "long" }).format(fechaActual);
 };
 
 const obtenerNombreDia = () => {
-    const fechaActual = new Date();
-    return new Intl.DateTimeFormat('es', { weekday: 'long' }).format(fechaActual);
+  const fechaActual = new Date();
+  return new Intl.DateTimeFormat("es", { weekday: "long" }).format(fechaActual);
 };
 
 const obtenerFechaNumero = () => {
-    const fechaActual = new Date();
-    const nombreMes = obtenerNombreMes();
-    return `${fechaActual.getDate().toString().padStart(2, '0')} ${nombreMes} ${fechaActual.getFullYear()}`;
+  const fechaActual = new Date();
+  const nombreMes = obtenerNombreMes();
+  return `${fechaActual
+    .getDate()
+    .toString()
+    .padStart(2, "0")} ${nombreMes} ${fechaActual.getFullYear()}`;
 };
 
 export {
-    obtenerFechaFormateada,
-    obtenerNombreMes,
-    obtenerNombreDia,
-    obtenerFechaNumero,
+  obtenerFechaFormateada,
+  obtenerNombreMes,
+  obtenerNombreDia,
+  obtenerFechaNumero,
 };
